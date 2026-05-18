94b21092499eb76379e1ed03f621c9de1c4dfebb
wajdan <wejdan95.almamari@gmail.com>
2026-05-18 16:00:24 +0400

    Register New Guest

--- HotelManagementSystem/Program.cs
+++ HotelManagementSystem/Program.cs
@@ -23,9 +23,93 @@ namespace HotelManagementSystem
             bool isGuestRegist = false;
 
             //rejon 2 : system processing ( menu functions )
+            bool exit =false;
+            while(exit == false)
+            {
+
+            
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("-------Hotel Management System-------");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("   0.Register New Guest");
+            Console.WriteLine("   1.View Guest Information");
+            Console.WriteLine("   2.Check-In Guest");
+            Console.WriteLine("   3.Check-Out & Bil");
+            Console.WriteLine("   4.Apply Discount");
+            Console.WriteLine("   5.Upgrade Room");
+            Console.WriteLine("   6.Add Room Service Note");
+            Console.WriteLine("   7.Search Guest by Name");
+            Console.WriteLine("   8.Calculate Loyalty Points");
+            Console.WriteLine("   9.Print Receipt");
+            Console.WriteLine("   10.Edit Guest Name");
+            Console.WriteLine("   11.Exit");
+            Console.WriteLine("-------------------------------------");
+            Console.Write(" Enter your choice: ");
+
+            int EnterChoise = Convert.ToInt32(Console.ReadLine());
+
+                switch (EnterChoise)
+                {
+                    case 0://0.Register New Guest
+                        Console.WriteLine("-------------------------------------");
+                        Console.WriteLine("   0.Register New Guest");
+                        Console.WriteLine("-------------------------------------");
+                        Console.Write("Enter gust Name : ");
+                        gName = Console.ReadLine().Trim();
+                        Console.Write("Enter gust Phone : ");
+                        gPhone = Console.ReadLine().Trim();
+                        Console.Write("Enter Room Type (S/D/F) : ");
+                        roomType=char.Parse(Console.ReadLine());
+                        Console.Write("Enter your Nightly Rate : ");
+                        nightlyRate = Convert.ToInt32(Console.ReadLine());
+                        Random random = new Random();
+                        roomNum = random.Next(1, 20);
+
+                        isGuestRegist = true;
+
+                        Console.WriteLine("Guest registered successfully.");
+                        Console.WriteLine("Room Number: " + roomNum);
+                        
+                        
+                        break;
+                    case 1://1.View Guest Information
+                        Console.WriteLine();
+                        break;
+                    case 2:// 2.Check-In Guest
+                        Console.WriteLine();
+                        break;
+                    case 3://3.Check-Out & Bil
+                        Console.WriteLine();
+                        break;
+                    case 4:// 4.Apply Discount
+                        Console.WriteLine();
+                        break;
+                    case 5://5.Upgrade Room
+                        Console.WriteLine();
+                        break;
+                    case 6://6.Add Room Service Note
+                        Console.WriteLine();
+                        break;
+                    case 7://7.Search Guest by Name
+                        Console.WriteLine();
+                        break;
+                    case 8:// 8.Calculate Loyalty Points
+                        Console.WriteLine();
+                        break;
+                    case 9://9.Print Receipt
+                        Console.WriteLine();
+                        break;
+                    case 10://10.Edit Guest Name
+                        Console.WriteLine();
+                        break;
+                    case 11://11.Exit
+                        Console.WriteLine();
+                        break;
+                }//end of switch
 
 
 
+            }//end of while
         }
     }
 }
