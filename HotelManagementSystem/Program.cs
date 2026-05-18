310fca2c9574af3c7851ad16758ff948718bec20
wajdan <wejdan95.almamari@gmail.com>
2026-05-18 15:08:57 +0400

    rejon1: system storage ( variables )

---
+++ HotelManagementSystem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HotelManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //rejon 1 : system storage ( variables )
+            string gName = "";
+            string gPhone = "";
+            int roomNum = 0;
+            char roomType = ' ';
+            double nightlyRate = 0;
+            string checkinDate = "";
+            string checkoutDate = "";
+            int numOfNight = 0;
+            string roomNote = "";
+            double discountPercentage = 0;
+            int loyaltyPoints = 0;
+            bool isGuestRegist = false;
+
+            //rejon 2 : system processing ( menu functions )
+
+
+
+        }
+    }
+}
