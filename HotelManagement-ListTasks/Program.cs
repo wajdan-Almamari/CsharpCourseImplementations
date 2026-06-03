4e529943b639dc305044e8ce2356169690da1d52
wajdan <wejdan95.almamari@gmail.com>
2026-06-03 11:58:21 +0400

    Create Hotel Management List Practice 2 projects

---
+++ HotelManagement-ListTasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelManagement_ListTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
