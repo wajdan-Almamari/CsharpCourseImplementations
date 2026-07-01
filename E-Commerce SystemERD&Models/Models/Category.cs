993529d11d4be133b70af0a6f53ec7d91edf5e59
wajdan <wejdan95.almamari@gmail.com>
2026-07-01 16:33:10 +0400

    Add Category model

---
+++ E-Commerce SystemERD&Models/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace E_Commerce_SystemERD_Models.Models
+{
+    [Index(nameof(categoryName), IsUnique = true)]
+    public class Category
+    {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int categoryId { get; set; } // system generated
+
+            [Required]
+            [MaxLength(100)]
+            public string categoryName { get; set; } // user input
+
+            [MaxLength(500)]
+            public string? description { get; set; } // user input
+
+            [MaxLength(300)]
+            public string? imageUrl { get; set; } // user input
+        }
+    }
+}
